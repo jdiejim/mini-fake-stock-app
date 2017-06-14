e0ab92aac814c1459253f7d7153d7388a2d3b6d7
Juan Diego Jimenez <jdiejim@gmail.com>
2017-06-14 11:49:28 -0600

    add constants dir with URL file for fetching, add styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
+import { URL } from './constants/URL';
 import './App.css';
 
+const Header = styled.section`
+  background-color: blue;
+  margin: 20px auto;
+  width: 100px;
+  height: 200px;
+`;
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firms: [],
+      data: [],
+    }
+  }
+
+  componentDidMount() {
+    this.downloadMarketData();
+  }
+
+  downloadMarketData() {
+    fetch(URL)
+    .then(response => response.json())
+    .then(market => {
+      const { firms, data } = market;
+      this.setState({ firms, data });
+    })
+    .catch(e => console.log(e));
+  }
+
   render() {
     return (
       <div className="App">
+        <Header>
+          trolololo
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
@@ -13,6 +46,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+      </Header>
       </div>
     );
   }
---
+++ src/constants/URL.js
@@ -0,0 +1,1 @@
+export const URL = 'https://gist.githubusercontent.com/jdiejim/7104218ac75972364627de15a4880442/raw/065ab00392598fe3ba5cb148aab73badd8e13c35/.json';
